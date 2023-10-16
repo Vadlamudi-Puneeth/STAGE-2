e881d7fbe20ca461b833967d035fc66471807dd5
Vadlamudi Puneeth <118372146+Vadlamudi-Puneeth@users.noreply.github.com>
2023-10-16 20:01:27 +0530

    Update Lecture9II-CipherSchools.cpp

---
+++ Lecture9II-CipherSchools.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int sum(int,int);
+
+int main(){
+
+    int a = 3;
+    int b  = 5;
+    int add_sum = sum(a,b);
+    cout<<a<<endl;
+    cout<<add_sum;
+    cout<<endl<<b;
+    return 0;
+}
+int sum(int a,int b){
+    int c;
+    a = 10;
+    b = 11;
+    c = a + b;
+}
